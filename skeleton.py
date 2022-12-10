0d6ddc47006c7be79d2e87393b302b2d5f907093
chefsif <wasif.m.pervez@gmail.com>
2022-12-10 16:45:11 -0500

    implemented __str__ and generate_password

--- skeleton.py
+++ skeleton.py
@@ -1,3 +1,6 @@
+import random
+import string
+
 def main():        
     """ Runs the main execution loop for the password manager program. Performs
         different operations and functions of the User and Password classes
@@ -15,11 +18,11 @@ def main():
     pass
 
 class User:
-    """ Represents a user with several stored paasswords.
+    """ Represents a user with several stored passwords.
     
     Attributes:
         name (string): the user's name
-        password_list {account (string) : password (password) }: a dictionary 
+        password_list {account (string) : password (Password)}: a dictionary 
             storing key/value pairs of accounts and passwords, respectively.
     """
     
@@ -32,7 +35,8 @@ class User:
         Side effects:
             Initializes the value of password_list to an empty dictionary.
         """
-        pass
+        self.name = name
+        self.password_list = {}
     
     def update_password(self, website, new_pass):
         """ Updates the password for a certain website the user has a password 
@@ -49,7 +53,8 @@ class User:
     
         Returns:
             the new value for the key website in the dictionary
-        """     
+        """
+        pass    
 
     def check_security(account):
         """ Calculates a security score for a password.
@@ -75,9 +80,16 @@ class User:
         Side effects:
             Modidies values in the attribute password_list.
         """
-        pass
+        new_password = ""
+        all_chars = [string.ascii_lowercase, string.ascii_uppercase,
+                string.digits, string.punctuation]
+        for character in range(30):
+            # randomly decide what type of character to choose, then choose
+            # which random character of that type to insert in the password
+            new_password += random.choice(all_chars[random.randint(0,3)])
+        return new_password
     
-    def __str__():
+    def __str__(self):
         """ Returns an informal string representation of a User object.
             Prompts user whether to display accounts sorted by password age or
             alphabetically.
@@ -87,7 +99,39 @@ class User:
                 accounts and corresponding passwords. 
         """
         # Techniques Demonstrated: magic methods / sorting with lambda function
-        pass
+        user_string = ""
+        user_string += self.name + "\n"
+
+        # asking user whether to sort by password age or alphabetically by 
+        #   account
+        option_chosen = False
+        while (not option_chosen):
+            criteria = input("Please enter the word 'age' if you would like " 
+                + "to sort by age or the word 'alpha' if you would like to "
+                + "sort alphabetically by account/website name."
+            )
+
+            if criteria != "age" and criteria != "alpha":
+                print("Please try again.")
+            else:
+                option_chosen = True
+        
+        # sorts password_list based on user input
+        if(criteria == "alpha"):
+            sorted_passwords = sorted(self.password_list, 
+                key = lambda account: account
+            )
+        if(criteria == "age"):
+            sorted_passwords = sorted(self.password_list, 
+                key = lambda account: self.password_list[account].age
+            )
+        
+        # assembles informal string representation based on sorted list
+        for password in sorted_passwords:
+            user_string += (f"Account: {password} -" + 
+                f"Password: {self.password_list[password]}\n")
+            
+        return user_string.strip()
     
     def import_passwords(filepath):
         """ Populates the user's password list by reading accounts and 
@@ -109,29 +153,31 @@ class Password:
     
     Attributes:
         passcode (string): actual password
-        age (int): how long the account has had the password in hours
+        age (int): how long the account has had the password in days
     """  
-    def __init__(self, passcode, age=0):
+    def __init__(self, passcode, age=1):
         """ Initializes a Password object.
         
         Args:
             passcode (string): the actual text of the password.
-            age (int): how long the password has been in use. 
-                Defaults to 0 hours.
+            age (int): how long the password has been in use.
+                Defaults to 1 day.
             
         """
         # Technique Demonstrated: optional parameter
-        pass
+        self.passcode = passcode
+        self.age = age
     
-    def edit_password(self, new_password, age=0):
+    def edit_password(self, new_password, age=1):
         """ Changes the value and age of a password.
         
         Args:
             new_password (string): the new text of the password.
             age (int): how long the new password has been in use. 
-                Defaults to 0 hours.
+                Defaults to 1 day.
         """
-        pass
+        self.passcode = new_password
+        self.age = age
 
 if __name__ == "__main__":
     main()
